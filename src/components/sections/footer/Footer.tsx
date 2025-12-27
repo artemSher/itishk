91a868dfe44470e305131d8a0d67af0ff5270a5a
poplopok <smirnovvigev@gmail.com>
2025-12-27 21:40:49 +0300

    fix address

--- src/components/sections/footer/Footer.tsx
+++ src/components/sections/footer/Footer.tsx
@@ -46,13 +46,11 @@ export const Footer = () => {
                 <Mail className={styles.contactIcon} size={20} />
                 <a href="mailto:itishkino@yandex.ru">itishkino@yandex.ru</a>
               </li>
-
               <li className={styles.contactItem}>
                 <MapPin className={styles.contactIcon} size={20} />
-
                 <div className="contactTexts">
                   <a
-                    href="https://yandex.ru/maps/?text=%D0%B3.%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D0%9C%D0%B0%D1%80%D1%8C%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9%20%D0%B1%D1%83%D0%BB%D1%8C%D0%B2%D0%B0%D1%80%2C%204"
+                    href="https://yandex.ru/maps/-/CLHTYZyw"
                     target="_blank"
                     rel="noopener noreferrer"
                     className={styles.contactLink}
@@ -61,12 +59,12 @@ export const Footer = () => {
                   </a>{" "}
                   <br />
                   <a
-                    href="https://yandex.ru/maps/?text=%D0%B3.%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%203-%D0%B5%20%D0%BF%D0%BE%D1%87%D1%82%D0%BE%D0%B2%D0%BE%D0%B5%20%D0%BE%D1%82%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%2C%2082"
+                    href="https://yandex.ru/maps/-/CLHTY4yO"
                     target="_blank"
                     rel="noopener noreferrer"
                     className={styles.contactLink}
                   >
-                    г. Москва, ул. 3-е почтовое отделение, д. 82
+                    г. Люберцы, ул. 3-е почтовое отделение, д. 82
                   </a>
                 </div>
               </li>
